e83460aedb50c83e145b9839b6f31958bf941f8d
Angella Mulikatete <mulikateteangella@gmail.com>
2025-05-07 00:23:34 +0300

    modified campaigns model

--- src/models/Campaign.js
+++ src/models/Campaign.js
@@ -5,15 +5,39 @@ const CampaignSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    media:{
+        type:[String] //links to images/files etc
+    },
     description: String,
+
+    rewardType:{
+        type: String,
+        enum: ['USDC', 'Token', 'Points'],
+        default: 'Points'
+    },
     rewardAmount: {
         type: Number,
         required: true
     },
+    creator: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User'
+    },
     category: String,
+    status: {
+        type: String,
+        enum: ['active', 'paused', 'ended'],
+        default: 'active'
+    },
     active:{
         type:Boolean,
         default: true
+    },
+    startDate: {
+        type: Date
+    },
+    endDate: {
+        type: Date
     },
     createdAt: {
         type: Date,
