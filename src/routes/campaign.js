1aaaa29ea617da8a0cf692c5c0ca8b90fb8371f6
Angella Mulikatete <mulikateteangella@gmail.com>
2025-05-07 01:21:24 +0300

    modified campaign routes

--- src/routes/campaign.js
+++ src/routes/campaign.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const router = express.Router();
 
-const {getCampaigns, createCampaign} = require('../controllers/campaign');
+const {getCampaigns, createCampaign, getCampaignById} = require('../controllers/campaign');
 
-router.route('/')
-.get(getCampaigns)
-.post(createCampaign);
+// router.route('/')
+// .get(getCampaigns)
+// .post(createCampaign);
+
+router.post('/', createCampaign);
+
+// Get all campaigns
+router.get('/', getCampaigns);
+
+// Get a single campaign by ID
+router.get('/:id', getCampaignById);
 
 module.exports = router;
